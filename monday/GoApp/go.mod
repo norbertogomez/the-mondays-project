1e9ae1d9a50c3e705f47384b1925c77a2e60d23c
Norberto <norbertogomezbonnin@gmail.com>
2021-04-19 15:53:06 +0200

    Add monitoring to GoApp (#3)
    Co-authored-by: Norberto Gomez <norberto.gomezbonnin@metro-markets.de>

--- monday/GoApp/go.mod
+++ monday/GoApp/go.mod
@@ -1,8 +1,8 @@
-module feedManager
+module mondays
 
 go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/stretchr/testify v1.4.0
+	github.com/prometheus/client_golang v1.10.0
 )
--- monday/GoApp/main.go
+++ monday/GoApp/main.go
@@ -2,7 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
+	"math/rand"
+	"time"
+)
+
+var (
+	counter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "mondays_app_http_health_counter",
+		Help: "Counter for counting amount of requests to health endpoint",
+	})
+
+	histogram = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "mondays_app_http_sample_histogram",
+		Help: "Sample histogram for Mondays project",
+	})
 )
 
 func main()  {
@@ -10,14 +27,34 @@ func main()  {
 	srv := gin.Default()
 
 	srv.GET("/health", func(context *gin.Context) {
+		log.Println("new health request")
+		counter.Inc()
 		context.JSON(200, gin.H{
 			"status": "Ok",
 		})
 	})
 
+	srv.GET("/metrics", prometheusHandler())
+
 	err := srv.Run()
+
+	go func() {
+		for {
+			rand.Seed(time.Now().UnixNano())
+			histogram.Observe(float64(rand.Intn(100-0+1) + 0))
+			time.Sleep(1 * time.Second)
+		}
+	}()
 
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
